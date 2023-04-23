dc9cd7d03e97357cff52d6e0e115ac01d7560507
Mahmoud Marayef <mahmoudmarayef@gmail.com>
2023-04-23 05:15:12 +0200

    api

--- littlelemon/urls.py
+++ littlelemon/urls.py
@@ -2,8 +2,15 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
+from rest_framework import routers
+from restaurant import views
+
+router = routers.DefaultRouter()
+router.register(r'tables', views.BookingViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('restaurant/', include('restaurant.urls'))
+    path('restaurant/', include('restaurant.urls')),
+    path('restaurant/menu/',include('restaurant.urls')),
+    path('restaurant/booking/', include(router.urls)),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
